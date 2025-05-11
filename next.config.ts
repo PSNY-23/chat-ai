757638517380f7642ecf62675896640b05688932
pankaj <pankajteceract@gmail.com>
2025-05-11 14:13:13 +0530

    Add the pathname in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'randomuser.me' },
-      { protocol: 'https', hostname: 'upload.wikimedia.org' },
-      { protocol: 'https', hostname: 'uploads-ssl.webflow.com' }
+      { protocol: "https", hostname: "randomuser.me", pathname: "/api/portraits/**" },
+      { protocol: "https", hostname: "upload.wikimedia.org" },
+      { protocol: "https", hostname: "uploads-ssl.webflow.com" },
     ],
   },
 };
